9f367e5d44f4c7aad183f052c502b1d8c6371e1f
wodoame <bmwodoame@gmail.com>
2026-06-14 22:42:04 +0000

    feat: add Spring Boot bootstrap app with health check endpoint

---
+++ todoapp/src/test/java/com/wodoame/todoapp/TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wodoame.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
